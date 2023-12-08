458e61bb522272abb1c26c98ea62e700a10a2598
Happy Little Engineers <86372652+ftc19374@users.noreply.github.com>
2023-12-07 16:19:49 -0800

    Add files via upload

--- DC_Center_Stage_TestV2.java
+++ DC_Center_Stage_TestV2.java
@@ -1,115 +1,148 @@
 package org.firstinspires.ftc.teamcode;
+/*Tele-Op Program for the Center Stage Competition. Functions include:
+Robot Movement (Forward, Backward, Strafing, Turning)
+Intake mechanism (conveyor)
+Lift Control
+Box (open/close)
+Drone Launch
+Hanging mechanism (Not Implemented at current time)
+ */
+import android.media.MediaCodecInfo;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.I2cAddr;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.TouchSensor;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.CRServo;
 
 @TeleOp(name="DC_Center_Stage_TestV2")
 
 public class DC_Center_Stage_TestV2 extends LinearOpMode{
-    DcMotor Front_Right_Wheel = null;
-    DcMotor Front_Left_Wheel  = null;
-    DcMotor Back_Right_Wheel  = null;
-    DcMotor Back_Left_Wheel   = null;
-    DcMotor Intake_Motor_1    = null;
-    DcMotor Lift_Motor_1      = null;
-    DcMotor Box_Motor_1       = null;
-    DcMotor Drone_Motor_1     = null;
-    CRServo   Drone_Servo_1     = null;
-    float Power = 0;
-    float LPower = 0;  //lift power variable
-    float BPower = 0;  //Box power variable
-    float DroneS = 0; //Servo for the Drone (Power)
-    float DroneM = 0; //Motor for the drone (Power)
+    DcMotor Front_Right_Wheel = null; //Dc Motor Variable for Front Right Wheel
+    DcMotor Front_Left_Wheel  = null; //Dc Motor Variable for Front Left Wheel
+    DcMotor Back_Right_Wheel  = null; //Dc Motor Variable for Back Right Wheel
+    DcMotor Back_Left_Wheel   = null; //Dc Motor Variable for Back Left Wheel
+    DcMotor Intake_Motor    = null; //Dc Motor Variable for Intake Motor
+    DcMotor Lift_Motor      = null; //Dc Motor Variable for Lift Motor
+    Servo   Box_Servo       = null; //180 degree Servo Variable for Box Mechanism
+    DcMotor Drone_Motor     = null; //Dc Motor Variable for Drone Launch Mechanism
+    CRServo   Drone_Servo   = null; //Continuous Servo Variable for Drone Launch Mechanism
+    DcMotor Hang_Motor      = null; //Dc Motor Variable for Hanging Mechanism
+    double IPower = 0; //Intake power variable
+    float LPower = 0;  //Lift power variable
+    float BPosition = 0;  //Box position variable
+    float DroneS = 0; //Servo Power Variable for Drone
+    float DroneM = 0; //Motor Power Variable for Drone
+    float HPower = 0; //Hanging mechanism Power Variable
     @Override
     public void runOpMode() {
-        Front_Right_Wheel = hardwareMap.get(DcMotor.class, "Fr_motor");
-        Front_Left_Wheel  = hardwareMap.get(DcMotor.class, "Fl_motor");
-        Front_Left_Wheel.setDirection(DcMotorSimple.Direction.REVERSE);
-        Back_Right_Wheel  = hardwareMap.get(DcMotor.class, "Br_motor");
-        Back_Left_Wheel   = hardwareMap.get(DcMotor.class, "Bl_motor");
-        Back_Left_Wheel.setDirection(DcMotorSimple.Direction.REVERSE);
-        Intake_Motor_1    = hardwareMap.get(DcMotor.class, "Intake_Motor_1");
-        Lift_Motor_1      = hardwareMap.get(DcMotor.class, "Lift_1");
-        Box_Motor_1       = hardwareMap.get(DcMotor.class, "Box_1");
-        Drone_Motor_1     = hardwareMap.get(DcMotor.class, "Drone_Motor");
-        Drone_Servo_1     = hardwareMap.get(CRServo.class, "Drone_Servo");
 
+        //Config names below
+        Front_Right_Wheel = hardwareMap.get(DcMotor.class, "Fr_motor");
+        Front_Right_Wheel.setDirection(DcMotorSimple.Direction.REVERSE);
+        Front_Left_Wheel  = hardwareMap.get(DcMotor.class, "Fl_motor");
+        Back_Right_Wheel  = hardwareMap.get(DcMotor.class, "Br_motor");
+        Back_Right_Wheel.setDirection(DcMotorSimple.Direction.REVERSE);
+        Back_Left_Wheel   = hardwareMap.get(DcMotor.class, "Bl_motor");
+        Intake_Motor    = hardwareMap.get(DcMotor.class, "Intake_Motor_1");
+        Lift_Motor      = hardwareMap.get(DcMotor.class, "Lift_1");
+        Box_Servo       = hardwareMap.get(Servo.class, "Box_Servo");
+        Drone_Motor     = hardwareMap.get(DcMotor.class, "Drone_Motor");
+        Drone_Servo     = hardwareMap.get(CRServo.class, "Drone_Servo");
+        Hang_Motor      = hardwareMap.get(DcMotor.class, "Hang_Motor");
+        Lift_Motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Lift_Motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        telemetry.addData("Pos: ", Lift_Motor.getCurrentPosition());
+        telemetry.update();
         waitForStart();
         while(opModeIsActive()) {
-            double y = -gamepad1.left_stick_y; //reverses the y axis allowing for only forward and backwards movement
+
+            //Equations for Robot wheels for the left & right joysticks on gamepad 1
+            double y = gamepad1.left_stick_y;
             double x = gamepad1.left_stick_x;
             double rx = gamepad1.right_stick_x;
 
 
-            Front_Right_Wheel.setPower(-(y + x - rx)); //front right wheel always goes backwards on new driver control station when not inverted
-            Front_Left_Wheel.setPower((y - x + rx));
-            Back_Left_Wheel.setPower((y + x + rx));
-            Back_Right_Wheel.setPower(-(y - x - rx));
-
-            if (rx > 0 || rx < 0) {
-            }
+            Front_Right_Wheel.setPower(y + x + rx);
+            Front_Left_Wheel.setPower((y - x - rx));
+            Back_Left_Wheel.setPower((y + x -  rx));
+            Back_Right_Wheel.setPower(y - x + rx);
 
             //5th motor controls intake via the left and right bumpers
-            // right bumper pulls in pixels
+            // right bumper on gamepad 1 adds power to intake motor to draw in pixels
             if (gamepad1.right_bumper) {
-                Power += 0.1;  //adds intake speed
-                Intake_Motor_1.setPower(Power);
-                sleep(250);
+                IPower = 1;  //Starts intake Mechanism; pulls pixels in
+                Intake_Motor.setPower(IPower);
             }
-            //left bumper pushes pixels out to prevent control of too many pixels at once
+            //left bumper on gamepad 1 pushes pixels out to prevent control of too many pixels at once
             else if (gamepad1.left_bumper) {
-                Power -= 0.1;  //subtracts intake speed
-                Intake_Motor_1.setPower(Power);
-                sleep(250);
+                IPower = -1;  //Reverses intake Mechanism; pushes pixels out
+                Intake_Motor.setPower(IPower);
             }
             if (gamepad1.x) {
-                Power = 0;  //stops intake
-                Intake_Motor_1.setPower(Power);
-                sleep(500);
+                //x on the gamepad 1 changes the power in the intake motor to 0, stopping the intake motor
+                IPower = 0;  //stops intake
+                Intake_Motor.setPower(IPower);
             }
             //raise and lower the lift to be able to reach higher on the backdrop
-            if (gamepad2.left_trigger >= 0.5) { //left trigger on gamepad 2 moves the lift up
-                LPower = 1; //test speed then change appropriately
-                Lift_Motor_1.setPower(LPower);
-                sleep(250);
-            } else if (gamepad2.right_trigger >= 0.5) { //right trigger on gamepad 2 moves lift down
-            LPower = -1; //test speed then change appropriately
-            Lift_Motor_1.setPower(LPower);
-            sleep(250);
+            if (gamepad2.left_trigger >= 0.5 && Lift_Motor.getCurrentPosition() > 0) {
+                //left trigger on gamepad 2 moves the lift up & includes position statements to prevent lift from going too far up
+                LPower = 1;
+                Lift_Motor.setPower(LPower);
+            } else if (gamepad2.right_trigger >= 0.5 && Lift_Motor.getCurrentPosition() <= 2950) {
+                //right trigger on gamepad 2 moves lift down
+            LPower = -1;
+            Lift_Motor.setPower(LPower);
+            }else{
+            Lift_Motor.setPower(0);
             }
+
+
             //Open and close the box function to drop pixels into backdrop
-            if (gamepad2.left_bumper) {  //left bumper on gamepad 2 opens the box
-                BPower = 0.5f;
-                Box_Motor_1.setPower(BPower);
+            if (gamepad2.left_bumper) {
+                //left bumper on gamepad 2 opens the box
+                BPosition = 90;
+                Box_Servo.setPosition(BPosition);
                 sleep(250);
             }
-            if (gamepad2.right_bumper) {  //right bumper on gamepad 2 closes the box
-                BPower = -0.5f;
-                Box_Motor_1.setPower(BPower);
-                sleep(250);
-            }
-            //Launch the drone from the robot using a servo and a motor
-            if (gamepad2.dpad_up) { //launches drone when pressed
-                DroneS = 1;
-                DroneM += 0.4;
-                Drone_Motor_1.setPower(DroneM);
-                Drone_Servo_1.setPower(DroneS);
-            } else if (gamepad2.dpad_down); { //turns off/stops the launcher
-                DroneS = 0;
-                DroneM -= 0.4;
-                Drone_Motor_1.setPower(DroneM);
-                Drone_Servo_1.setPower(DroneS);
+            if (gamepad2.right_bumper) {
+                //right bumper on gamepad 2 closes the box
+               BPosition = 0;
+               Box_Servo.setPosition(BPosition);
 
             }
-            //Add code for the Hanging function for the robot
+            //Launch the drone from the robot using a servo and a motor
+            if (gamepad2.dpad_right) {
+                //launches drone when pressed using the right dpad
+                DroneM = 0.5f;
+                Drone_Motor.setPower(DroneM);
+                sleep(300);
+                DroneS = 1;
+                Drone_Servo.setPower(DroneS);
+                sleep(300);
+                DroneM = 0;
+                DroneS = 0;
+                Drone_Motor.setPower(DroneM);
+                Drone_Servo.setPower(DroneS);
+            }
+            if (gamepad2.dpad_up) {
+                //Raises the hanging lift using up on the dpad
+                HPower = 0.5f; //Check Speed
+                Hang_Motor.setPower(HPower);
+            }
+            else if (gamepad2.dpad_down) {
+                //Lowers the Hanging lift using down on the dpad
+                HPower = -0.5f; //Check Speed
+                Hang_Motor.setPower(HPower);
+            }
+
+
+
+
+
+            telemetry.addData("Pos: ", Lift_Motor.getCurrentPosition()); //adds telemetry data to the Driver Station
+            telemetry.update();
         }
     }
 }
